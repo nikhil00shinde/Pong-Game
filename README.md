efe9fd78067ecfa1379afa353875659bb1604fc7
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-20 17:47:16 +0530

    scoring

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1 align="center">
+  Pong Game
+</h1>
+
+# Description:
+- Classic game of Pong
+- It is a two player game 
+- The first player to miss 3 of his/her balls will lose the match.
+
+![Overview](./img1.png)
+
+<br>
+
+## Screenshots
+
+![Screenshot1](./img2.png)
+
+![Screenshot2](./img3.png)
--- script.js
+++ script.js
@@ -6,6 +6,21 @@ let rightPaddle = document.querySelector(".right");
 let paddle = document.querySelector(".paddle");
 let boardBound = board.getBoundingClientRect();
 let x = (y = true);
+let leftPlayerLives = (rightPlayerLives = 3);
+
+// selectLivesIcon
+let leftBalls = document.querySelectorAll(".l1");
+let rightBalls = document.querySelectorAll(".r1");
+
+// change ball color
+function changeBallColor(ball) {
+	if (ball === "left") {
+		leftBalls[leftPlayerLives].classList.add("active");
+	} else {
+		rightBalls[rightPlayerLives].classList.add("active");
+	}
+}
+
 //taking user input
 document.addEventListener("keydown", (e) => {
 	if (e.key == "w") {
@@ -29,6 +44,11 @@ function movePaddle(cPaddle, change) {
 		cPaddle.style.top = cPaddleBound.top + change + "px";
 	}
 }
+function reset() {
+	ball.style.top = window.innerHeight * 0.45 + "px";
+	ball.style.left = window.innerWidth * 0.49 + "px";
+	requestAnimationFrame(moveBall);
+}
 
 function moveBall() {
 	let ballcord = ball.getBoundingClientRect();
@@ -36,6 +56,33 @@ function moveBall() {
 	let ballLeft = ballcord.left;
 	let ballRight = ballcord.right;
 	let ballBottom = ballcord.bottom;
+
+	// check if collided with any players horizontal boundary
+	let hasTouchedLeft = ballLeft < boardBound.left;
+	let hasTouchedRight = ballRight > boardBound.right;
+
+	if (hasTouchedLeft || hasTouchedRight) {
+		if (hasTouchedLeft) {
+			leftPlayerLives--;
+			changeBallColor("left");
+			if (leftPlayerLives == 0) {
+				alert("GAME OVER Player B !!!WON!!!");
+				document.location.reload();
+			} else {
+				return reset();
+			}
+		} else {
+			rightPlayerLives--;
+			changeBallColor("right");
+
+			if (rightPlayerLives == 0) {
+				alert("GAME OVER Player A !!!WON!!!");
+				document.location.reload();
+			} else {
+				return reset();
+			}
+		}
+	}
 
 	//handle vertically
 	if (ballTop <= boardBound.top || ballBottom >= boardBound.bottom) {
@@ -46,39 +93,35 @@ function moveBall() {
 	// if (ballLeft <= boardBound.left || ballRight >= boardBound.right) {
 	// 	x = !x;
 	// }
-   
+
 	// **********************COLLISION*******************
 	// right hit => np
 	let rightpaddleBounds = rightPaddle.getBoundingClientRect();
 
 	if (
-		rightpaddleBounds.top <= ballTop+30 &&
-		rightpaddleBounds.bottom >= ballBottom-30 &&
+		rightpaddleBounds.top <= ballTop + 30 &&
+		rightpaddleBounds.bottom >= ballBottom - 30 &&
 		rightpaddleBounds.left <= ballRight &&
 		rightpaddleBounds.right >= ballLeft
-	)
-	{
+	) {
 		x = !x;
 	}
-
 
 	// right hit => np
 	let leftpaddleBounds = leftPaddle.getBoundingClientRect();
 
 	if (
-		leftpaddleBounds.top <= ballTop+30 &&
-		leftpaddleBounds.bottom >= ballBottom-30 &&
+		leftpaddleBounds.top <= ballTop + 30 &&
+		leftpaddleBounds.bottom >= ballBottom - 30 &&
 		leftpaddleBounds.left <= ballRight &&
 		leftpaddleBounds.right >= ballLeft
-	)
-	{
+	) {
 		x = !x;
 	}
-   // *************************************COLLISION**************
+	// *************************************COLLISION**************
 
-    //issue 
+	//issue
 
-	
 	ball.style.top = y == true ? ballTop + 2 + "px" : ballTop - 2 + "px";
 	ball.style.left = x == true ? ballLeft + 2 + "px" : ballLeft - 2 + "px";
 	requestAnimationFrame(moveBall);
